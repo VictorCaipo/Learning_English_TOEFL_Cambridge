c1fa63103a595cc9fb1dbeab9c1bb1850a64bd05
Victor Caipo <caipoccoa@gmail.com>
2025-11-30 18:21:16 -0500

    modified:   FirstStory.c 	../README.md

---
+++ Writting/FirstStory.c
@@ -0,0 +1,21 @@
+#include <stdint.h>
+void story(void){
+/*
+The story was about a girl talking about her grandmother. She explain the her favorite item
+is a necklace because it was from her dad's mother who had experimented loss of memories.
+Once, the grandmother stayed at the girl's home, later, when she left the home she had forgotten
+her necklace. The girl isn't sure if it the necklace was a gift or she was just forgotten it.
+But it doesn't matter, the girl always use it because it was from her grandmother.
+*/
+/*
+The story was about a gril talking about her grandmother. She explained that her favorite item
+is a necklace because it was from her dad's mother who had experienced memory loss.
+Once, the grandmother stayed at the girl's home, and when she left, she had forgotten her necklace.
+The girl isn't sure if the necklace was a gift or if her grandmother just forgot it. But it doesn't
+matter; the girl always wears it becaus it was from her grandmother.
+*/
+
+
+}
+
+int main(void){}
